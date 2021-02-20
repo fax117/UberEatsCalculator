c9b4edd3562a2f69dce0e568de7e636f048115d5
Fabricio <fabricio.fuentes12@gmail.com>
2021-02-20 14:18:53 -0600

    Third commit

---
+++ app/src/main/java/com/example/ubereatscalculator/OrderClass.kt
@@ -0,0 +1,30 @@
+package com.example.ubereatscalculator
+
+import android.text.Editable
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
+
+data class OrderClass(var subtotal: Float){
+    var small_fee: Float = subtotal * 0.02F
+    var service_fee: Float = subtotal * 0.05F
+    var delivery_fee: Float = subtotal * 0.10F
+    var tip: Float = 0.0F
+    var total_final: Float = 0.0F
+    var notes: String = ""
+}
+
+fun main(){
+    var order1 = OrderClass(subtotal = 8.95F)
+
+    order1.tip = order1.subtotal * 0.10F // tip depends on the user
+
+    order1.total_final = order1.subtotal + order1.small_fee + order1.service_fee + order1.delivery_fee + order1.tip
+
+    println(order1)
+    println(order1.subtotal)
+    println(order1.small_fee)
+    println(order1.service_fee)
+    println(order1.delivery_fee)
+    println(order1.tip)
+    println(order1.total_final)
+}
